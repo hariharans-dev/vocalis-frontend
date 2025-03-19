450bbebb54591ea557bcc4916a498057f6db7290
hariharans-dev <hariharans.developer@gmail.com>
2025-03-19 09:25:52 +0000

    root - deleted

--- app/root/dashboard/orders/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>orders</h1>
-    </div>
-  );
-}
--- app/root/dashboard/people/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>people</h1>
-    </div>
-  );
-}
