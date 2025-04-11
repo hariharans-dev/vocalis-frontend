dbc9d95b5ac359cdffd412974658779ca9f880e3
hariharan s <hariharans.developer@gmail.com>
2025-04-11 17:02:24 +0530

    stable version

--- app/api/auth/Signup.ts
+++ app/api/auth/Signup.ts
@@ -1,5 +1,5 @@
 import { APIRequestOptions, fetchData } from "../FetchData";
-import { setToken } from "@/app/_api/Session";
+import { setToken } from "@/app/api/Session";
 
 interface ApiResponse {
   data?: {
